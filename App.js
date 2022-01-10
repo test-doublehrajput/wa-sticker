2284362c0bc504a80bb03b0603cf780796bb6f2d
Kishor Jena <ksr.jena@gmail.com>
2022-01-11 03:06:25 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import HomeScreen from './src/screens/HomeScreen';
+import DetailsScreen from './src/screens/DetailsScreen';
+
+const Stack = createNativeStackNavigator();
+
+//  this contains navigation  only 
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'rgb(59,108,212)',
+    fontSize: 42,
+    fontWeight: '600',
+    textAlign: 'center',
+  },
+})
